9073deba359698df6b1279523f904205a42b5614
Gary N <51265386+garyn90@users.noreply.github.com>
2020-01-24 18:29:52 -0500

    Update pocketDungeon.js

--- pocketDungeon.js
+++ pocketDungeon.js
@@ -33,7 +33,7 @@ $('#d6').on('click', function() {
     d6n++;    
     let counter = 'x' + d6n;
     d6Array.push(dice(6));
-    $('#d6textCounter').text(counter)
+    $('#d6textCounter').text(counter);
     console.log(counter);
     console.log(d6Array); 
 });
@@ -42,7 +42,7 @@ $('#d8').on('click', function() {
     d8n++;    
     let counter = 'x' + d8n;
     d8Array.push(dice(8));
-    $('#d8textCounter').text(counter)
+    $('#d8textCounter').text(counter);
     console.log(counter);
     console.log(d8Array);   
 });
@@ -50,7 +50,7 @@ $('#d8').on('click', function() {
 $('#d10').on('click', function() {
     d10n++;    
     let counter = 'x' + d10n;
-    $('#d10textCounter').text(counter)
+    $('#d10textCounter').text(counter);
     d10Array.push(dice(10));
     console.log(counter);
     console.log(d10Array);    
@@ -60,7 +60,7 @@ $('#d12').on('click', function() {
     d12n++;    
     let counter = 'x' + d12n;
     d12Array.push(dice(12));
-    $('#d12textCounter').text(counter)
+    $('#d12textCounter').text(counter);
     console.log(counter);
     console.log(d12Array);     
 });
@@ -69,9 +69,57 @@ $('#d20').on('click', function() {
     d20n++;    
     let counter = 'x' + d20n;
     d20Array.push(dice(20));
-    $('#d20textCounter').text(counter)
+    $('#d20textCounter').text(counter);
     console.log(counter);
     console.log(d20Array);     
 });
+
+
+//Button roll//
+$("#roll-button").on("click", function() {
+    $("#textarea").val(d4Array + " " + d6Array + " " + d8Array + " " + d10Array + " " + d12Array + " " + d20Array);
+      d4Array = [];
+      d6Array = [];
+      d8Array = [];
+      d10Array = [];
+      d12Array = [];
+      d20Array = [];
+      d4n = 0;
+      d6n = 0;
+      d8n = 0;
+      d10n = 0;
+      d12n = 0;
+      d20n = 0;
+      $('#d4textCounter').text(d4n);
+      $('#d6textCounter').text(d6n);
+      $('#d8textCounter').text(d8n);
+      $('#d10textCounter').text(d10n);
+      $('#d12textCounter').text(d12n);
+      $('#d20textCounter').text(d20n);
+});
+
+
+//Reset button//
+$("#reset-button").on("click", function() {
+      d4Array = [];
+      d6Array = [];
+      d8Array = [];
+      d10Array = [];
+      d12Array = [];
+      d20Array = [];
+      d4n = 0;
+      d6n = 0;
+      d8n = 0;
+      d10n = 0;
+      d12n = 0;
+      d20n = 0;
+      $('#d4textCounter').text(d4n);
+      $('#d6textCounter').text(d6n);
+      $('#d8textCounter').text(d8n);
+      $('#d10textCounter').text(d10n);
+      $('#d12textCounter').text(d12n);
+      $('#d20textCounter').text(d20n);
+});
+
 
 });
