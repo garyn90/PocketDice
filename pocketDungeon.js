e4b75cf4a4bfed2bd554c3c40b95d8a2736e434b
Gary N <51265386+garyn90@users.noreply.github.com>
2020-01-23 19:18:38 -0500

    Update and rename pocketdungeon.js to pocketDungeon.js

--- pocketDungeon.js
+++ pocketDungeon.js
@@ -1,30 +1,19 @@
 $(document).ready(function() {
 console.log("Awaiting orders...");
 
-$("#container").animate({height: "80px"}, "medium");
-$("#logo").delay(400).animate({left: ".3%"});
-$("#desc").delay(700).fadeIn(400);
-$("#beta").delay(700).fadeIn(400);
-$("#tweety").delay(700).fadeIn(400);
-$("#the-sheet, #second-sheet").delay(400).animate({left: "250px"}, "slow");
-
 //the modular dice function//
 function dice(num) {
     var math = Math.floor((Math.random() * num) + 1);
     return(math);
 }
 
-//Draggable test//
-$('#the-sheet, #second-sheet').draggable( {
-    appendTo: "body",
-    scrollSpeed: 600,
-    snap: false,
-});
-
-
-
 //dice counter and click events//
-var diceArray = [];
+var d4Array = [];
+var d6Array = [];
+var d8Array = [];
+var d10Array = [];
+var d12Array = [];
+var d20Array = [];
 var d4n = 0;
 var d6n = 0;
 var d8n = 0;
@@ -34,49 +23,55 @@ var d20n = 0;
 $('#d4').on('click', function() {
     d4n++;    
     let counter = 'x' + d4n;
-    diceArray.push(dice(4));
+    d4Array.push(dice(4));
+    $('#d4textCounter').text(counter);
     console.log(counter);
-    console.log(diceArray);    
+    console.log(d4Array);    
 });
 
 $('#d6').on('click', function() {
     d6n++;    
     let counter = 'x' + d6n;
-    diceArray.push(dice(6));
+    d6Array.push(dice(6));
+    $('#d6textCounter').text(counter)
     console.log(counter);
-    console.log(diceArray); 
+    console.log(d6Array); 
 });
 
 $('#d8').on('click', function() {
     d8n++;    
     let counter = 'x' + d8n;
-    diceArray.push(dice(8));
+    d8Array.push(dice(8));
+    $('#d8textCounter').text(counter)
     console.log(counter);
-    console.log(diceArray);   
+    console.log(d8Array);   
 });
 
 $('#d10').on('click', function() {
     d10n++;    
     let counter = 'x' + d10n;
-    diceArray.push(dice(10));
+    $('#d10textCounter').text(counter)
+    d10Array.push(dice(10));
     console.log(counter);
-    console.log(diceArray);    
+    console.log(d10Array);    
 });
 
 $('#d12').on('click', function() {
     d12n++;    
     let counter = 'x' + d12n;
-    diceArray.push(dice(12));
+    d12Array.push(dice(12));
+    $('#d12textCounter').text(counter)
     console.log(counter);
-    console.log(diceArray);     
+    console.log(d12Array);     
 });
 
 $('#d20').on('click', function() {
     d20n++;    
     let counter = 'x' + d20n;
-    diceArray.push(dice(20));
+    d20Array.push(dice(20));
+    $('#d20textCounter').text(counter)
     console.log(counter);
-    console.log(diceArray);     
+    console.log(d20Array);     
 });
 
 });
