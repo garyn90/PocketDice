559d181b7ee31f13e9e28ecc1d208a55c4fcba66
Gary N <51265386+garyn90@users.noreply.github.com>
2020-01-28 01:25:05 -0500

    Update pocketDungeon.js

--- pocketDungeon.js
+++ pocketDungeon.js
@@ -8,6 +8,7 @@ function dice(num) {
 }
 
 //dice counter and click events//
+
 var d4Array = [];
 var d6Array = [];
 var d8Array = [];
@@ -20,13 +21,13 @@ var d8n = 0;
 var d10n = 0;
 var d12n = 0;
 var d20n = 0;
+var allTheDice = [];
+
 $('#d4').on('click', function() {
     d4n++;    
     let counter = 'x' + d4n;
     d4Array.push(dice(4));
     $('#d4textCounter').text(counter);
-    console.log(counter);
-    console.log(d4Array);    
 });
 
 $('#d6').on('click', function() {
@@ -34,8 +35,6 @@ $('#d6').on('click', function() {
     let counter = 'x' + d6n;
     d6Array.push(dice(6));
     $('#d6textCounter').text(counter);
-    console.log(counter);
-    console.log(d6Array); 
 });
 
 $('#d8').on('click', function() {
@@ -43,8 +42,6 @@ $('#d8').on('click', function() {
     let counter = 'x' + d8n;
     d8Array.push(dice(8));
     $('#d8textCounter').text(counter);
-    console.log(counter);
-    console.log(d8Array);   
 });
 
 $('#d10').on('click', function() {
@@ -52,8 +49,6 @@ $('#d10').on('click', function() {
     let counter = 'x' + d10n;
     $('#d10textCounter').text(counter);
     d10Array.push(dice(10));
-    console.log(counter);
-    console.log(d10Array);    
 });
 
 $('#d12').on('click', function() {
@@ -61,8 +56,6 @@ $('#d12').on('click', function() {
     let counter = 'x' + d12n;
     d12Array.push(dice(12));
     $('#d12textCounter').text(counter);
-    console.log(counter);
-    console.log(d12Array);     
 });
 
 $('#d20').on('click', function() {
@@ -70,32 +63,61 @@ $('#d20').on('click', function() {
     let counter = 'x' + d20n;
     d20Array.push(dice(20));
     $('#d20textCounter').text(counter);
-    console.log(counter);
-    console.log(d20Array);     
 });
-
 
 //Button roll//
 $("#roll-button").on("click", function() {
-    $("#textarea").val(d4Array + " " + d6Array + " " + d8Array + " " + d10Array + " " + d12Array + " " + d20Array);
-      d4Array = [];
-      d6Array = [];
-      d8Array = [];
-      d10Array = [];
-      d12Array = [];
-      d20Array = [];
-      d4n = 0;
-      d6n = 0;
-      d8n = 0;
-      d10n = 0;
-      d12n = 0;
-      d20n = 0;
-      $('#d4textCounter').text(d4n);
-      $('#d6textCounter').text(d6n);
-      $('#d8textCounter').text(d8n);
-      $('#d10textCounter').text(d10n);
-      $('#d12textCounter').text(d12n);
-      $('#d20textCounter').text(d20n);
+  let diceString = '';
+  let userModifiers = $('#prof-select option:selected').val() + $('#abili-select').val() + $('#other-select').val();
+  console.log(userModifiers);
+  d4Array.forEach(function(item) {
+    allTheDice.push(item);
+  });
+  d6Array.forEach(function(item) {
+    allTheDice.push(item);
+  });
+  d8Array.forEach(function(item) {
+    allTheDice.push(item);
+  });
+  d10Array.forEach(function(item) {
+    allTheDice.push(item);
+  });
+  d12Array.forEach(function(item) {
+    allTheDice.push(item);
+  });
+  d20Array.forEach(function(item) {
+    allTheDice.push(item);
+  });  
+
+//event that turns all dice rolls & modifiers into presentable text for user
+  for (let i = 0; i < allTheDice.length; i++) {
+    if(i === allTheDice.length - 1 ) {
+      diceString += allTheDice[i] + ' =';
+    }
+    else {
+      diceString += allTheDice[i] + ' + ';
+    }
+    $('#textarea').val(diceString);   
+  }
+    d4Array = [];
+    d6Array = [];
+    d8Array = [];
+    d10Array = [];
+    d12Array = [];
+    d20Array = [];
+    allTheDice = [];
+    d4n = 0;
+    d6n = 0;
+    d8n = 0;
+    d10n = 0;
+    d12n = 0;
+    d20n = 0;
+    $('#d4textCounter').text(d4n);
+    $('#d6textCounter').text(d6n);
+    $('#d8textCounter').text(d8n);
+    $('#d10textCounter').text(d10n);
+    $('#d12textCounter').text(d12n);
+    $('#d20textCounter').text(d20n);
 });
 
 
@@ -107,6 +129,7 @@ $("#reset-button").on("click", function() {
       d10Array = [];
       d12Array = [];
       d20Array = [];
+      allTheDice = [];
       d4n = 0;
       d6n = 0;
       d8n = 0;
@@ -119,7 +142,12 @@ $("#reset-button").on("click", function() {
       $('#d10textCounter').text(d10n);
       $('#d12textCounter').text(d12n);
       $('#d20textCounter').text(d20n);
+      $('#textarea').val('');
 });
 
 
 });
+
+
+//extraneous stuff for rolls
+
